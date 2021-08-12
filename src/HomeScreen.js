ee3dc39b609cd18f15230638c0698adf410e498e
Madhav-Raverkar <madhavr111@gmail.com>
2021-08-12 17:26:56 +0530

    Project Committed

---
+++ src/HomeScreen.js
@@ -0,0 +1,28 @@
+import React from "react";
+import Banner from "./Banner";
+import "./HomeScreen.css";
+import Nav from "./Nav";
+import Row from "./Row";
+import requests from "./requests";
+
+
+function HomeScreen(){
+    return <div className="homeScreen">
+         
+        <Nav/>
+
+        <Banner/>
+
+        <Row title='Netflix Originals' fetchURL={requests.fetchNetflixOriginals} isLargeRow />
+        <Row title='Top Rated' fetchURL={requests.fetchTopRated} isLargeRow />
+        <Row title='Action Movies' fetchURL={requests.fetchActionMovies} isLargeRow />
+        <Row title='Comedy Movies' fetchURL={requests.fetchComedyMovies} isLargeRow />
+        <Row title='Horror Movies' fetchURL={requests.fetchHorrorMovies} isLargeRow />
+        <Row title='Romance Movies' fetchURL={requests.fetchRomanceMovies} isLargeRow />
+        <Row title='Documentaries' fetchURL={requests.fetchDocumentaries} isLargeRow />
+
+    </div>;
+
+}
+
+export default HomeScreen;
---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyA1tdP1qgCDWO9FLbYRkFVJLSGcF5nkzTA",
+    authDomain: "netflix-clone-780f0.firebaseapp.com",
+    projectId: "netflix-clone-780f0",
+    storageBucket: "netflix-clone-780f0.appspot.com",
+    messagingSenderId: "323277314770",
+    appId: "1:323277314770:web:0d42a2f4efdd42483feb0b"
+  };
